16e97c4017764851163e84309b152240098a8d91
igusvs <igoralves159@gmail.com>
2024-11-23 23:59:20 -0300

    Incluir testes unitarios usecase

--- src/test/java/com/example/demo/adapter/gateway/interfaces/impl/RecuperarClienteAdapterTest.java
+++ src/test/java/com/example/demo/adapter/gateway/interfaces/impl/RecuperarClienteAdapterTest.java
@@ -13,8 +13,6 @@ import org.mockito.MockitoAnnotations;
 import java.util.Optional;
 
 import static org.assertj.core.api.Assertions.assertThat;
-import static org.junit.jupiter.api.Assertions.*;
-import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.ArgumentMatchers.anyString;
 import static org.mockito.Mockito.*;
 
